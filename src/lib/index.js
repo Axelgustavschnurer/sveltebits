e22936f92d704ec6ecfb91c0d52ff809183ec0e0
Axel <axel.schnurer@gmail.com>
2023-07-18 20:27:40 +0200

    Add exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,3 +2,7 @@
 export { default as Header } from './headers/header.svelte';
 export { default as Footer } from './footers/footer.svelte';
 export { default as Hamburger } from './menus/hamburger.svelte';
+export { default as Nav } from './navigation/nav.svelte';
+export { default as CallToAction } from './links/callToAction.svelte';
+export { default as Logo } from './images/logo.svelte';
+export { default as Map } from './maps/map.svelte'
